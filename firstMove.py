6d833c8e4911c758bdec29750d20edaeaab13132
Stephen Clark <sclark@linux.com>
2017-02-04 15:32:05 -0500

    firstMove function to assign jobs on first move

---
+++ firstMove.py
@@ -0,0 +1,70 @@
+class Jobs:
+    SCOUT = 0
+    COLLECTOR = 1
+
+class JobRegions:
+    W = 0
+    NW = 1
+    N = 2
+    NE = 3
+    E = 4
+    SE = 5
+    S = 6
+    SW = 7
+
+nScouts = 8*2
+
+def firstMove(self, view):
+
+    # Check W (cuz west is best)
+    if view[2][1][1] < nScouts/8:
+        self.job = Jobs.SCOUT
+        self.jobRegion = JobRegions.W
+        return (Actions.MOVE_W, Actions.DROP_NONE)
+    
+    # Check NW
+    elif view[1][1][1] < nScouts/8:
+        self.job = Jobs.SCOUT
+        self.jobRegion = JobRegions.NW
+        return (Actions.MOVE_NW, Actions.DROP_NONE)
+    
+    # Check N
+    elif view[1][2][1] < nScouts/8:
+        self.job = Jobs.SCOUT
+        self.jobRegion = JobRegions.N
+        return (Actions.MOVE_N, Actions.DROP_NONE)
+    
+    # Check NE
+    elif view[1][3][1] < nScouts/8:
+        self.job = Jobs.SCOUT
+        self.jobRegion = JobRegions.NE
+        return (Actions.MOVE_NE, Actions.DROP_NONE)
+        
+    # Check E
+    elif view[2][3][1] < nScouts/8:
+        self.job = Jobs.SCOUT
+        self.jobRegion = JobRegions.E
+        return (Actions.MOVE_E, Actions.DROP_NONE)
+        
+    # Check SE
+    elif view[3][3][1] < nScouts/8:
+        self.job = Jobs.SCOUT
+        self.jobRegion = JobRegions.SE
+        return (Actions.MOVE_SE, Actions.DROP_NONE)
+        
+    # Check S
+    elif view[3][2][1] < nScouts/8:
+        self.job = Jobs.SCOUT
+        self.jobRegion = JobRegions.S
+        return (Actions.MOVE_S, Actions.DROP_NONE)
+        
+    # Check SW
+    elif view[3][1][1] < nScouts/8:
+        self.job = Jobs.SCOUT
+        self.jobRegion = JobRegions.SW
+        return (Actions.MOVE_SW, Actions.DROP_NONE)
+
+    # Guess you're just a collector
+    else:
+        self.job = Jobs.COLLECTOR
+        return (Actions.DROPOFF, Actions.DROP_NONE)
