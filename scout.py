50cccf657307f3e154c8818d7ce06491aa928f4a
Grant Campfield <goc@andrew.cmu.edu>
2017-02-04 15:55:37 -0500

    Code for scout to move in a relatively straight line

---
+++ scout.py
@@ -0,0 +1,44 @@
+from __future__ import division, print_function
+# functions to be used by the scout robot
+import random
+import math
+
+zig_size = 2
+num_scouts = 16
+
+def round_away(x):
+    # round x away from 0
+    if x == 0: return round(random.random()) * 2 - 1
+
+    if x < 0: x = math.floor(x)
+    else: x = math.ceil(x)
+
+    if x > 1: return 1
+    if x < -1: return -1
+    return x
+
+def next_move((x, y), direction, turn):
+    # returns the next step, disregarding mountains
+    angle = (direction / num_scouts) * 2 * math.pi
+
+    optimal_x = math.cos(angle) * turn
+    optimal_y = math.sin(angle) * turn
+
+    move_x = optimal_x - x
+    move_y = optimal_y - y
+
+    print(round(move_x), round(move_y), end=" | ")
+    return (round_away(move_x), round_away(move_y))
+
+def test_next():
+    # test next move function
+    for d in range(num_scouts):
+        # print("=" * 25 + " " + str(d) + " " + "=" * 25)
+        curr = (0, 0)
+        for i in range(1, 1000):
+            move = next_move(curr, d, i)
+            # print(move, end=" => ")
+            assert(-1 <= move[0] <= 1)
+            assert(-1 <= move[1] <= 1)
+            curr = (curr[0] + move[0], curr[1] + move[1])
+            # print(curr)
